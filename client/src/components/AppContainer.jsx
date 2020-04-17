2e60006f9fa6f537e839d25f8cbcfd795f9d1598
house472 <morehouse472@hotmail.com>
2020-04-16 20:25:30 -0500

    MERNCW_final

--- client/src/components/AppContainer.jsx
+++ client/src/components/AppContainer.jsx
@@ -9,12 +9,69 @@ class AppContainer extends Component {
             characterArray: []
         }
     }
+
+    
+
+    componentDidMount = () =>{
+        // this.state.characterArray.push({
+        //     name: "Charlie Brown",
+        //     gender: "Male",
+        //     age: 8
+        // });
+
+        // this.state.characterArray.push({
+        //     name: "Sheldon Cooper",
+        //     gender: "Male",
+        //     age: 24
+        // });
+        // this.setState({characterArray : this.state.characterArray});
+        this.loadData();
+    }
+    loadData = async ()=>{
+        let response =await fetch('/api')
+        // console.log(response);
+        let json = await response.json();
+        console.table(json)
+        this.setState({characterArray :json})
+    }
+
+
+    handleChange = (event) => {
+        if (event.target.name === "name") {
+            this.setState({ name: event.target.value })
+        } else if (event.target.name === "gender") {
+            this.setState({ gender: event.target.value })
+        } else if (event.target.name === "age") {
+            this.setState({ age: event.target.value })
+        }
+    }
+
+
+    handleSubmission = async (event) => {
+        event.preventDefault();
+        let formData = {
+            name: this.state.name,
+            gender: this.state.gender,
+            age: this.state.age
+        }
+        let response = await fetch('/api', {
+            method: "POST",
+            headers : {
+                'Accept' :'application/json',
+                "Content-type" : 'application/json'
+            },
+            body : JSON.stringify(formData)
+        })
+        let json = await response.json();
+        // console.log(this.state)
+    }
+
     render() {
         return (
             <div>
                 <h1>Simple MERN Application</h1>
-                <form action="">Characters
-            <label htmlFor="name">Character Name</label>
+                <form action="">
+                    <label htmlFor="name">Character Name</label>
                     <input type="text" name="name" value={this.state.name} onChange={this.handleChange} />
 
                     <label htmlFor="gender">Gender</label>
@@ -27,13 +84,16 @@ class AppContainer extends Component {
                 </form>
                 <div>
                     {
-                        this.state.characterArray.map(name, index) =>{
-                            return(
-                                <div>
-                                    
+                        this.state.characterArray.map((name, index) => {
+                            return (
+                                <div key={index}>
+                                    Name:{name.name}
+                                    Gender:{name.gender}
+                                    Age:{name.age}
+
                                 </div>
                             )
-                        }
+                        })
                     }
                 </div>
 
